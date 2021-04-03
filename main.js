506079c8f2077545a4a7da1ccd57f3b0297ae1a1
lotfi aouane <lotfiaouane@gmail.com>
2021-04-03 13:23:16 +0100

    last version

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let human = 0;
-let computer = 0;
-let result = "";
+const restart = document.getElementById('restart');
+const container = document.querySelector('#roundResult');
+const paragraph = document.createElement('h2');
+const modal = document.querySelector('.modal');
+const winner = document.getElementById('winner');
+const loser = document.getElementById('loser');
+
+container.appendChild(paragraph);
+const scoreboard = {
+    player: 0,
+    computer: 0
+};
 
 function computerPlay() {
+
     let response = "";
     let output = Math.floor((Math.random() * 3) + 1);
     switch (output) {
@@ -19,56 +29,121 @@ function computerPlay() {
 }
 
 
-function playRound(HumanPlay, computerSelection) {
+function playRound(humanPlay, computerSelection) {
+    restart.style.display = 'inline-block';
     let playerSelection = "";
-    let HumanSelection = "";
-    if (typeof HumanPlay === 'string') {
-        HumanSelection = HumanPlay.toLowerCase();
-        playerSelection = HumanSelection.charAt(0).toUpperCase() + HumanSelection.slice(1);
+    if (humanPlay === 'rock') {
+        playerSelection = "Rock";
+    } else if (humanPlay === 'paper') {
+        playerSelection = "Paper";
+    } else {
+        playerSelection = "Scissors";
     }
 
     if (playerSelection === "Rock" && computerSelection === "Rock") {
-        console.log(`Draw \n Human ${human} vs computer ${computer}`)
+        displayResults(`Null Rock vs Rock  `, paragraph);
     } else
     if (playerSelection === "Rock" && computerSelection === "Scissors") {
-        human++;
-        console.log(`You win,Rock beats Scissors \n Human ${human} vs computer ${computer} `);
+        scoreboard.player++;
+
+        displayResults(`You win, Rock beats Scissors  `, paragraph);
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-        computer++;
-        console.log(`You lose, Paper beats Rock \n Human ${human} vs computer ${computer}`);
+        scoreboard.computer++;
+
+        displayResults(`You lose, Paper beats Rock  `, paragraph);
     } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        console.log(`Draw \n Human ${human} vs computer ${computer}`)
+        displayResults(`Null Paper vs Paper  `, paragraph);
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-        human++;
-        console.log(`You win, Paper beats Rock \n Human ${human} vs computer ${computer}`);
+        scoreboard.player++;
+
+        displayResults(`You win, Paper beats Rock  `, paragraph);
     } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-        computer++;
-        console.log(`You lose,Scissors beats Paper \n Human ${human} vs computer ${computer}`);
+        scoreboard.computer++;
+
+        displayResults(`You lose, Scissors beats Paper  `, paragraph);
     } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-        human++;
-        console.log(`You win,Scissors beats Paper \n Human ${human} vs computer ${computer}`);
+        scoreboard.player++;
+
+        displayResults(`You win, Scissors beats Paper  `, paragraph);
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        computer++;
-        console.log(`You lose,Rock beats Scissors \n Human ${human} vs computer ${computer} `);
+        scoreboard.computer++;
+
+        displayResults(`You lose, Rock beats Scissors  `, paragraph);
     } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        console.log(`Draw \n Human ${human} vs computer ${computer}`)
+        displayResults(`Null Scissors vs Scissors  `, paragraph);
     }
+
 
 
 }
 
-function game() {
-    let round = 5;
-    for (let i = 0; i < round; i++) {
-        playRound(prompt(), computerPlay());
-    }
-    if (human > computer) {
-        return "human win"
-    } else if (human < computer) {
-        return "computer win"
+function updateScoreBoard() {
+    // Show score
+    score.innerHTML = `
+<h2>Player: ${scoreboard.player}</h2>
+<h2>Computer: ${scoreboard.computer}</h2>
+`;
+}
+
+function displayResults(str, element) {
+    if (scoreboard.player === 5 || scoreboard.computer === 5) {
+        declareWinner();
     } else {
-        return "No winner"
+
+        element.textContent = str;
+        updateScoreBoard();
     }
-    human = 0;
-    computer = 0;
 }
+
+function declareWinner() {
+    if (scoreboard.player > scoreboard.computer) {
+        modal.style.display = 'block';
+        loser.style.display = 'none'
+
+    } else {
+        modal.style.display = 'block';
+        winner.style.display = 'none'
+
+    }
+
+}
+
+
+function restartGame() {
+    scoreboard.computer = 0;
+    scoreboard.player = 0;
+    paragraph.textContent = '';
+    restart.style.display = 'none';
+    updateScoreBoard()
+}
+
+function replayGame() {
+    modal.style.display = 'none';
+    loser.style.display = 'block';
+    winner.style.display = 'block'
+    restartGame()
+}
+
+
+const replayButton = document.querySelectorAll("span");
+
+replayButton.forEach((button) => {
+
+    button.addEventListener('click',
+        () => {
+            replayGame();
+        })
+})
+
+const buttons = document.querySelectorAll('i');
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+
+    button.addEventListener('click',
+        () => {
+            playRound(button.id, computerPlay());
+        })
+})
+
+restart.addEventListener('click', () => restartGame());
